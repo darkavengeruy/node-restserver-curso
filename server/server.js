9e26d1807cbfa8cf462844fda147ea2bf9f436d5
FERNANDO FRANCO <ferfranco.uy@hotmail.com>
2019-05-27 01:29:54 -0400

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 
 const bodyParser = require('body-parser')
@@ -11,38 +13,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-});
 
-app.post('/usuario', function(req, res) {
+//mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useCreateIndex: true },
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
+        if (err) throw err;
 
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
+        console.log('Base de datos ONLINE');
 
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
     });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
